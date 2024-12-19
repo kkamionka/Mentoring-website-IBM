5b1cbb890dbc03ca21018124429e48a60b277888
Gabi-te <GabiNsmra@gmail.com>
2024-12-19 15:47:10 +0000

    83RD CHANGE

--- sign-in.php
+++ sign-in.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>sign-in | AGK Mentors</title>
-    <link rel="stylesheet" href="sign-in.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="sign-in.css">
 
 </head>
 <body>
     <div class="banner">
         <div class="navbar">
-            <a href="homepage.html">
+            <a href="">
                 <img src="images/logo-transparent-svg.svg" class="logo">
             </a>
             <ul>
                 <li><a href="homepage.html">Home</a></li> 
                 <li><a href="about-page.html">About Us</a></li>
                 <li><a href="services.html">Services</a></li>
-                <a href="log-in.php">
+                <a href="log-in.html">
                     <img src="images/account_circle_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24(1).png" class="icon" alt="Account Icon">
                 </a>
             </ul>
@@ -38,14 +38,18 @@
                 <input type="password" placeholder="">
                 <label>Confirm Password</label>
                 <input type="password" placeholder="">
-                <input  type="button" value="Submit">   
+                <!-- ADD DASHBOARD HERE -->
+                <a href="mentors.html">
+                    <input type="button" value="Submit">
+                </a>
+                
             </form>
             <p>By clicking the Sign Up button, you agree to our <br>
             <a href="#">Terms and Conditions</a> and <a href="#">Policy Privacy</a>   
             </p>
     
         </div>
-        <p>Already have an account? <a href="#">Login here</a></p>
+        <p class="para-2">Already have an account? <a href="log-in.html">Login here</a></p>
      
     </div>
 
